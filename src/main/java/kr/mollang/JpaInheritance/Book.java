ce189d97375b71444d91686ca521d88121d773e5
MHS <hayaruby7909@gmail.com>
2023-08-15 17:47:32 +0900

    Feat: 상속 추가

---
+++ src/main/java/kr/mollang/JpaInheritance/Book.java
@@ -0,0 +1,8 @@
+package kr.mollang.JpaInheritance;
+
+
+class Book extends Item{
+
+    private String auther;
+    private String isbn;
+}
---
+++ src/main/java/kr/mollang/JpaInheritance/Movie.java
@@ -0,0 +1,8 @@
+package kr.mollang.JpaInheritance;
+
+
+public class Movie extends Item{
+
+    private String director;
+    private String actor;
+}
